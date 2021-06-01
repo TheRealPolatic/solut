637b9f250f8ce723d7f3f11326b6b4cdb626b89b
DESKTOP-765QD4C\Jan de Sterke <jandesterke@hotmail.com>
2021-06-01 13:44:35 +0200

    First attempt on creating a component for the timeline

---
+++ components/impactgoal/impactgoal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="m-4 bg-gray-100 rounded-lg p-4">
+    <div class="row flex justify-between">
+      <div class="col text-sm">
+        We made impact on <span class="font-bold">5.914</span> people so far
+      </div>
+      <div class="col">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 0 24 24"
+          width="24px"
+          fill="#000000"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path
+            d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"
+          />
+        </svg>
+      </div>
+    </div>
+
+    <div class="row bg-gray-300 h-1 rounded-lg my-3 overflow-hidden">
+      <div class="col w-3/6 bg-black h-full"></div>
+    </div>
+
+    <div class="row flex justify-between items-center text-xs">
+      <div class="col font-semibold text-gray-400">
+        2.086 people till the next milestone
+      </div>
+      <div class="col bg-primary rounded-lg px-2 py-1 text-white font-semibold">
+        8.000
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ pages/timeline.vue
@@ -0,0 +1,11 @@
+<template>
+  <impactgoal></impactgoal>
+</template>
+
+<script>
+export default {
+  // page properties go here
+}
+</script>
+
+<style></style>
