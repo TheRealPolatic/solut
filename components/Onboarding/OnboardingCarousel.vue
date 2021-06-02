9bf91c8ef02c22efdce24f75057135b612fdf897
Bram <bram@owow.io>
2021-06-02 16:06:03 +0200

    feat: added more illustrations and onboarding screens

--- components/Onboarding/OnboardingCarousel.vue
+++ components/Onboarding/OnboardingCarousel.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-col justify-evenly h-screen overflow-hidden">
     <VueSlickCarousel :arrows="true" :dots="true" ref="carousel" class="flex flex-col justify-evenly">
+      <!-- slide 1 -->
       <div class="flex flex-col justify-evenly h-full">
         <h1 class="text-dark text-center font-bold text-3xl">
           Join the <br />
@@ -9,8 +10,18 @@
         <img class="w-7/8 mx-auto mb-14 mt-8" src="@/assets/illustrations/race-to-resilience.svg" />
         <p class="text-center mt-24">Putting people and nature first in <br />pursuit of a resilient world.</p>
       </div>
-      <div>2</div>
-      <div>3</div>
+      <!-- slide 2 -->
+      <div class="flex flex-col justify-evenly h-full">
+        <h1 class="text-dark text-center font-bold text-3xl">Solutions</h1>
+        <img class="w-7/8 mx-auto mb-14 mt-8" src="@/assets/illustrations/solution-illustration.svg" />
+        <p class="text-center mt-24">Becoming more resilient to <br />climate change as a community <br />by sharing solutions.</p>
+      </div>
+      <!-- slide 3 -->
+      <div class="flex flex-col justify-evenly h-full">
+        <h1 class="text-dark text-center font-bold text-3xl">Track impact</h1>
+        <img class="w-7/8 mx-auto mb-14 mt-8" src="@/assets/illustrations/impact-illustration.svg" />
+        <p class="text-center mt-24">Track the impact of all solutions. <br />We strive to impact 100.000 people <br />all around the world.</p>
+      </div>
     </VueSlickCarousel>
 
     <button class="mx-auto rounded-3xl bg-primary h-12 w-12 flex justify-center items-center" @click="showNext">
@@ -52,7 +63,7 @@ export default {
 <style>
 .slick-dots {
   background-color: transparent; /* bg color of container */
-  bottom: 80px;
+  bottom: 100px;
 }
 .slick-dots button:before {
   color: #00e3a8 !important; /* color of dots */
