9b7f41f49132c6e34561d2f383ca20bcf9c528e2
DESKTOP-765QD4C\Jan de Sterke <jandesterke@hotmail.com>
2021-06-08 11:26:48 +0200

    New icons and an improved configuration file

--- .fantasticonrc.js
+++ .fantasticonrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   inputDir: './assets/iconfont/icon-source', // (required)
   outputDir: './assets/iconfont/icon-dist', // (required)
-  fontTypes: ['ttf', 'svg'],
+  fontTypes: ['ttf', 'woff'],
   assetTypes: ['ts', 'css', 'json', 'html'],
+  descent: undefined,
+  fontHeight: 300,
   normalize: true,
 }
