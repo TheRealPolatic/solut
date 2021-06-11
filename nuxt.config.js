91347851e7d4d1268d599577576668e8e0281c77
Jan de Sterke <58807486+jcsdesterke@users.noreply.github.com>
2021-06-11 10:34:52 +0200

    Merging conflict solved

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  css: [],
+  css: ['@/assets/main.css', '@/assets/iconfont/icon-dist/icons.css'],
 
   plugins: ['~/plugins/firebase.js'],
 
