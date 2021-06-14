8c65688aaa4ef6878ecbcf632d999822b6808665
Jan de Sterke <58807486+jcsdesterke@users.noreply.github.com>
2021-06-14 11:37:22 +0200

    Timeline data, first version

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -2,14 +2,19 @@
   <div>
     <impactgoal></impactgoal>
     <div class="flex m-4">
-      <pill v-for="pill in pills" :key="pill.text" :pill="pill" />
+      <!-- <pill v-for="pill in pills" :key="pill.text" :pill="pill" /> -->
     </div>
     <solutioncard></solutioncard>
   </div>
 </template>
 
 <script>
+// import { mapState } from 'vuex'
+
 export default {
+  created() {
+    this.$store.dispatch('solution/fetchSolutions')
+  },
   data() {
     return {
       pills: [
@@ -22,6 +27,11 @@ export default {
         },
       ],
     }
+  },
+  computed: {
+    solutions() {
+      return this.$store.state.solution
+    },
   },
 }
 </script>
