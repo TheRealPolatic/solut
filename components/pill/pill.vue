2a8e11137cd03b50de461b75042e5a1e8d2bda2a
Jan de Sterke <58807486+jcsdesterke@users.noreply.github.com>
2021-06-15 11:05:01 +0200

    Solution card changes

--- components/pill/pill.vue
+++ components/pill/pill.vue
@@ -1,12 +1,8 @@
 <template>
-  <div class="py-1 px-3 bg-primary text-sm rounded-full flex items-center text-white text-medium">
+  <div class="py-1 px-3 bg-gray-200 text-sm rounded-full flex items-center text-black text-medium mr-2">
     <div class="mr-2">
-      <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill="white">
-        <path d="M0 0h24v24H0z" fill="none" />
-        <path d="M16 6l2.29 2.29-4.88 4.88-4-4L2 16.59 3.41 18l6-6 4 4 6.3-6.29L22 12V6z" />
-      </svg>
+      <i class="icon icon-smog"></i>
     </div>
-    <v-if pill.state="active">{{ pill.state }}</v-if>
     {{ pill.label }}
   </div>
 </template>
--- pages/timeline.vue
+++ pages/timeline.vue
@@ -2,25 +2,21 @@
   <div>
     <impactgoal></impactgoal>
     <div class="flex m-4">
-      <!-- <pill v-for="pill in pills" :key="pill.text" :pill="pill" /> -->
+      <pill v-for="pill in pills" :key="pill.text" :pill="pill" :class="{ 'bg-primary text-white': pill.active }" />
     </div>
-    <solutioncard></solutioncard>
+    <solutioncard v-for="card in content.solutions" :key="card.id" :carddata="card" />
   </div>
 </template>
 
 <script>
-// import { mapState } from 'vuex'
-
 export default {
-  created() {
-    this.$store.dispatch('solution/fetchSolutions')
-  },
   data() {
     return {
+      content: this.$store.state.solution,
       pills: [
         {
           label: 'Trending',
-          state: 'active',
+          active: true,
         },
         {
           label: 'Top',
@@ -32,6 +28,9 @@ export default {
     solutions() {
       return this.$store.state.solution
     },
+  },
+  created() {
+    this.$store.dispatch('solution/fetchSolutions')
   },
 }
 </script>
