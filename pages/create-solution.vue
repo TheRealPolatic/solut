cda35bc31daf550c0647c94f9392bf7dc59d4e99
TheRealPolatic <d.toirkens@student.fontys.nl>
2021-06-16 16:38:08 +0200

    refactor: replaced the uploading of the solution to the create solution page

--- pages/create-solution.vue
+++ pages/create-solution.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-    <!-- <transition name="slide-left" mode="out-in"> -->
-    <!-- <CreateBasicInfo v-if="index == 1" @addBasicInfo="addToSolution" />
-    <CreateMaterials v-else-if="index == 2" @addMaterials="addToSolution" @back="index--" />
-    <CreateAddSteps v-else-if="index == 3" @addSteps="addToSolution" @back="index--" />
-    <div v-else-if="index == 4">
-      <div @click="submitSolution" class="rounded-xl w-40 h-14 flex items-center justify-center mb-8 cursor-pointer bg-primary text-white">
-        <p>Submit</p>
+    <transition name="slide-left" mode="out-in">
+      <CreateBasicInfo v-if="index == 1" @addBasicInfo="addToSolution" />
+      <CreateMaterials v-else-if="index == 2" @addMaterials="addToSolution" @back="index--" />
+      <CreateAddSteps v-else-if="index == 3" @addSteps="addToSolution" @back="index--" />
+      <div v-else-if="index == 4">
+        <div @click="uploadSolution" class="rounded-xl w-40 h-14 flex items-center justify-center mb-8 cursor-pointer bg-primary text-white">
+          <p>Submit</p>
+        </div>
       </div>
-    </div> -->
 
-    <!-- <CreateAddSteps /> -->
-    <!-- <CreateMaterials /> -->
-    <CreateImpactModal />
-    <!-- </transition> -->
+      <!-- <CreateImpactModal /> -->
+    </transition>
   </div>
 </template>
 
 <script>
+import { firestore, storage } from '../plugins/firebase'
+
 export default {
   data() {
     return {
@@ -31,7 +31,74 @@ export default {
       this.index++
     },
     submitSolution() {
-      this.$store.dispatch('solution/createSolution', this.solution)
+      // Not needed since the solution uploading is in uploadSolution()
+      this.$store.dispatch('solution/createSolution', this.readySolution)
+    },
+    uploadSolution() {
+      console.log(this.solution)
+
+      const postData = {
+        title: this.solution.title,
+        introduction: this.solution.introduction,
+        categories: this.solution.categories,
+        materials: this.solution.materials,
+        tools: this.solution.tools,
+      }
+      let id
+      let ext
+      let blob
+      let r
+
+      firestore
+        .collection('solutions')
+        .add(postData)
+        .then((data) => {
+          id = data.id
+          return id
+        })
+        .then(async (id) => {
+          const filename = this.solution.image.name
+          ext = filename.slice(filename.lastIndexOf('.'))
+          r = await fetch(this.solution.image.blob)
+          blob = await r.blob()
+          return storage.ref(`solutions/${id}${ext}`).put(blob)
+        })
+        .then(() => {
+          return storage.ref(`solutions/${id}${ext}`).getDownloadURL()
+        })
+        .then((url) => {
+          return firestore.collection('solutions').doc(id).update({ imageUrl: url })
+        })
+        .then(async () => {
+          // https://stackoverflow.com/questions/52873516/vue-js-returns-ob-observer-data-instead-of-my-array-of-objects
+          let steps = JSON.parse(JSON.stringify(this.solution.steps))
+
+          let actions = steps.map((step) => {
+            return new Promise(async (resolve) => {
+              let stepData = {
+                'description': step.description,
+              }
+              if (step.stepImage.length) {
+                const filename = step.stepImage[0].name
+                ext = filename.slice(filename.lastIndexOf('.'))
+
+                r = await fetch(step.stepImage[0].blob)
+                blob = await r.blob()
+                await storage.ref(`solutions/${id}_${filename}`).put(blob)
+                let url = await storage.ref(`solutions/${id}_${filename}`).getDownloadURL()
+                console.log(url)
+                stepData['stepImage'] = url
+              }
+              resolve(stepData)
+            })
+          })
+          Promise.all(actions).then((data) => {
+            firestore.collection('solutions').doc(id).update({ steps: data })
+          })
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     },
   },
 }
