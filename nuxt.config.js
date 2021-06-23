f8a7677548a9b68904327f1d7d90e3f0ebade4ff
luuklijten <32705472+luuklijten@users.noreply.github.com>
2021-06-23 14:40:46 +0200

    changed title in manifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ssr: false,
 
   head: {
-    title: 'solut',
+    title: 'Solut',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
