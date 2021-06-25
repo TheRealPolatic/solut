463361a0696194a23fcb2ed0763a071808112133
Pim <pimvandenhurk@gmail.com>
2021-06-25 10:27:13 +0200

    change profile

--- pages/editProfile.vue
+++ pages/editProfile.vue
@@ -8,19 +8,57 @@
     </div>
 
     <!-- Change user info -->
-    <form id="profile-form" @submit.prevent="updateProfileInfo(userinfo.id, fieldValueUser, fieldValueEmail)">
+    <form id="profile-form" @submit.prevent="updateProfileInfo(userinfo.id, 'fieldValueUser', 'fieldValueEmail')">
       <!-- Image component -->
-      <UserProfileAvatarInput v-model="form.avatar" :default-src="userinfo.profileImage"></UserProfileAvatarInput>
+      <!-- <UserProfileAvatarInput :default-src="userinfo.profileImage" :userinfo="userinfo"></UserProfileAvatarInput> -->
+      <!-- Profile Image -->
+
+      <div class="flex justify-center">
+        <div class="flex justify-center items-center relative mt-2">
+          <div class="relative" v-if="!userinfo.profileImage.length">
+            <file-upload
+              ref="upload"
+              v-model="userinfo.profileImage"
+              @input-filter="inputFilter"
+              accept="image/*"
+              :size="1024 * 1024"
+              class="preview rounded-full h-24 w-24 bg-light-grey"
+            >
+            </file-upload>
+            <div class="h-8 w-8 absolute bottom-0 right-0 bg-primary rounded-full flex justify-center items-center border-2 border-white">
+              <i class="flex text-xs icon icon-plus text-white"></i>
+            </div>
+          </div>
+
+          <div class="relative" v-else>
+            <file-upload
+              ref="upload"
+              v-model="userinfo.profileImage"
+              @input-filter="inputFilter"
+              accept="image/*"
+              :size="1024 * 1024"
+              class="preview rounded-full h-24 w-24"
+              :style="{ 'background-image': `url(${userinfo.profileImage[0].blob})` }"
+            >
+            </file-upload>
+            <div class="h-8 w-8 absolute bottom-0 right-0 bg-primary rounded-full flex justify-center items-center border-2 border-white">
+              <i class="flex text-xs icon icon-edit text-white"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- v-model="form.avatar" -->
 
       <!-- Username -->
       <div class="mt-8">
         <label class="text-dark font-semibold">Username</label><br />
-        <input type="text" :placeholder="userinfo.username" v-model="fieldValueUser" class="border border-dark-grey rounded-xl h-12 w-full mt-2 pl-4 pr-4" />
+        <input type="text" :placeholder="userinfo.username" class="border border-dark-grey rounded-xl h-12 w-full mt-2 pl-4 pr-4" />
       </div>
       <!-- Email -->
       <div class="mt-8">
         <label class="text-dark font-semibold">Email</label><br />
-        <input type="text" :placeholder="userinfo.email" v-model="fieldValueEmail" class="border border-dark-grey rounded-xl h-12 w-full mt-2 pl-4 pr-4" />
+        <input type="text" :placeholder="userinfo.email" class="border border-dark-grey rounded-xl h-12 w-full mt-2 pl-4 pr-4" />
       </div>
 
       <Button :label="'Update profile'" class="mt-8"></Button>
@@ -38,18 +76,20 @@ export default {
   async asyncData({ params, store }) {
     const userinfo = store.state.user.user
 
+    console.log(userinfo)
+
     return { userinfo }
   },
   data() {
     return {
-      form: {
-        avatar: null,
-        username: fieldValueUser,
-        email: fieldValueEmail,
-      },
+      // form: {
+      //   avatar: null,
+      //   // username: fieldValueUser,
+      //   // email: fieldValueEmail,
+      // },
       userinfo: {},
-      fieldValueUser: this.form.username,
-      fieldValueEmail: this.form.email,
+      // fieldValueUser: this.form.username,
+      // fieldValueEmail: this.form.email,
     }
   },
 
@@ -60,6 +100,20 @@ export default {
       user
         .updateProfile({
           displayName: x,
+        })
+        .then(async () => {
+          let id
+          let ext
+          let blob
+          let r
+          this.user.uid = res.user.uid
+          const filename = this.user.profileImage[0].name
+          ext = filename.slice(filename.lastIndexOf('.'))
+          r = await fetch(this.user.profileImage[0].blob)
+          blob = await r.blob()
+          await storage.ref(`users/${this.user.uid}${ext}`).put(blob)
+
+          return await storage.ref(`users/${this.user.uid}${ext}`).getDownloadURL()
         })
         .then(() => {
           // Update successful
@@ -91,6 +145,21 @@ export default {
           document.querySelector('.error').innerHTML = error.message
           console.log(error.message)
         })
+    },
+    inputFilter(newFile, oldFile, prevent) {
+      if (newFile && !oldFile) {
+        // Filter non-image file
+        if (!/\.(jpeg|jpe|jpg|gif|png|webp)$/i.test(newFile.name)) {
+          return prevent()
+        }
+      }
+
+      // Create a blob field
+      newFile.blob = ''
+      let URL = window.URL || window.webkitURL
+      if (URL && URL.createObjectURL) {
+        newFile.blob = URL.createObjectURL(newFile.file)
+      }
     },
   },
 }
